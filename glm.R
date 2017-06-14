4f7332846adb6c1f0926471677d7438cc99809e9
Jeremy Funk <jeremy.m.funk@gmail.com>
2017-06-14 15:46:58 -0400

    updated glm

--- glm.R
+++ glm.R
@@ -1,14 +1,8 @@
 
 
 library(caret)
-
-
-library(car)
-library(boot)
-library(ggplot2)
-library(ROCR)
-library(Hmisc)
-library(dplyr)
+library(car)  # for vif
+library(boot)  # for cv.glm
 
 
 # read data
@@ -33,8 +27,8 @@ cost <- function(resp, score)  {
 intrain<-createDataPartition(y=df$income50k,p=0.2,list=FALSE)
 train<-df[intrain,]
 test<-df[-intrain,]
-table(train$DQ60.18m)
-table(test$DQ60.18m)
+table(train$income50k)
+table(test$income50k)
 
 
 # get model formula
@@ -46,43 +40,43 @@ options(contrasts = c("contr.treatment", "contr.poly"))
 m1 <- glm(income50k ~ 
             age
           + class.of.worker
-          + industry.code
-          + occupation.code
+          #+ industry.code
+          #+ occupation.code
           + education
-          + enrolled.in.edu.inst.last.wk
+          #+ enrolled.in.edu.inst.last.wk
           + marital.status
-          + major.industry.code
-          + major.occupation.code
+          #+ major.industry.code
+          #+ major.occupation.code
           + race
           + hispanic.Origin
           + sex
           + member.of.a.labor.union
-          + reason.for.unemployment
+          #+ reason.for.unemployment
           + full.or.part.time.employment.stat
           + capital.gains
           + capital.losses
           + divdends.from.stocks
           + tax.filer.status
-          + region.of.previous.residence
-          + state.of.previous.residence
-          + detailed.household.and.family.stat
-          + detailed.household.summary.in.household
-          + migration.code.change.in.msa
-          + migration.code.change.in.reg
-          + migration.code.move.within.reg
+          #+ region.of.previous.residence
+          #+ state.of.previous.residence
+          #+ detailed.household.and.family.stat
+          #+ detailed.household.summary.in.household
+          #+ migration.code.change.in.msa
+          #+ migration.code.change.in.reg
+          #+ migration.code.move.within.reg
           + live.in.this.house.1.year.ago
-          + migration.prev.res.in.sunbelt
+          #+ migration.prev.res.in.sunbelt
           + num.persons.worked.for.employer
-          + family.members.under.18
-          + country.of.birth.father
-          + country.of.birth.mother
-          + country.of.birth.self
-          + citizenship
+          #+ family.members.under.18
+          #+ country.of.birth.father
+          #+ country.of.birth.mother
+          #+ country.of.birth.self
+          #+ citizenship
           + own.business.or.self.employed
-          + fill.inc.questionnaire.for.veterans.admin
-          + veterans.benefits
+          #+ fill.inc.questionnaire.for.veterans.admin
+          #+ veterans.benefits
           + weeks.worked.in.year
-          , family=binomial(link='logit'), data=df)
+          , family=binomial(link='logit'), data=train)
 
 summary(m1)
 
@@ -92,5 +86,5 @@ options(contrasts=c("contr.sum","contr.poly"))
 d <- drop1(m1, ~., test="LRT") 
 d[order(d$AIC),]
 
-cv1 <- cv.glm(df, m1, cost, 3)
+cv1 <- cv.glm(train, m1, cost, 5)
 cv1$delta
